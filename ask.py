2e74b6dbd193561980334e20a79533dcc90b7838
pengfeng <pengfeng@gmail.com>
2024-10-17 07:42:15 -0700

    Update requirements.txt (#10)

--- ask.py
+++ ask.py
@@ -15,6 +15,7 @@ class Ask:
 
     def __init__(self, logger: Optional[logging.Logger] = None):
         self.read_env_variables()
+
         if logger is not None:
             self.logger = logger
         else:
@@ -26,7 +27,7 @@ class Ask:
 
         self.memory = Memory()
 
-    def read_env_variables(self):
+    def read_env_variables(self) -> None:
         err_msg = ""
 
         self.search_api_key = os.environ.get("SEARCH_API_KEY")
@@ -234,20 +235,20 @@ def search_extract_summarize(query: str, model_name: str, log_level: str):
     for i, link in enumerate(links):
         logger.debug(f"{i+1}. {link}")
 
-    logger.info("Scraping the URLs...")
+    logger.info("Scraping the URLs ...")
     scrape_results = ask.scrape_urls(links)
 
-    logger.info("Chunking the text...")
+    logger.info("Chunking the text ...")
     chunking_results = ask.chunk_results(scrape_results, 1000, 100)
     for url, chunks in chunking_results.items():
         logger.debug(f"URL: {url}")
         for i, chunk in enumerate(chunks):
             logger.debug(f"Chunk {i+1}: {chunk}")
 
-    logger.info("Saving to vector DB...")
+    logger.info("Saving to vector DB ...")
     ask.save_to_db(chunking_results)
 
-    logger.info("Querying the vector DB...")
+    logger.info("Querying the vector DB ...")
     results = ask.vector_search(query)
     for i, result in enumerate(results):
         logger.debug(f"{i+1}. {result}")
